20e49969e1faa2b5ae9268a0d9d199f04a45d0ff
Kai Hendry <kai.hendry@thoughtworks.com>
2023-08-02 09:28:56 +0100

    refactor: pre-commit hooks and apply comments on https://github.com/thoughtworks/maeve-csms/pull/8

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,28 @@
 #!/usr/bin/env bash
 
+command_exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
 BEARER_TOKEN=$(curl -s https://hubject.stoplight.io/api/v1/projects/cHJqOjk0NTg5/nodes/6bb8b3bc79c2e-authorization-token | jq -r .data | sed -n '/Bearer/s/^.*Bearer //p')
 
 # fall back to BEARER_TOKEN if no arg
 CSO_OPCP_TOKEN="${1:-$BEARER_TOKEN}"
 MO_OPCP_TOKEN="${2:-$BEARER_TOKEN}"
 
-export MO_OPCP_TOKEN=$MO_OPCP_TOKEN; export CSO_OPCP_TOKEN=$CSO_OPCP_TOKEN;docker-compose up "${@:2}" --build
+shift
+
+# Check if 'docker compose' is available (with space)
+if command_exists "docker compose"; then
+  DOCKER_COMPOSE_CMD="docker compose"
+else
+  # Check if 'docker-compose' is available
+  if command_exists docker-compose; then
+    DOCKER_COMPOSE_CMD="docker-compose"
+  else
+    echo "Error: Neither 'docker-compose' nor 'docker compose' is available. Please install Docker Compose."
+    exit 1
+  fi
+fi
+
+export MO_OPCP_TOKEN=$MO_OPCP_TOKEN; export CSO_OPCP_TOKEN=$CSO_OPCP_TOKEN;$DOCKER_COMPOSE_CMD up "${@:2}"
