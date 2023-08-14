ea7dfcc8428edf059e136d234c5b04dc9ae28dc6
Gesa Stupperich <gesa.stupperich@gmail.com>
2023-08-14 21:01:02 +0100

    fix: instrument CertificateSigned handler with Open Telemetry instead of slog

--- manager/handlers/has2be/certificate_signed_result.go
+++ manager/handlers/has2be/certificate_signed_result.go
@@ -4,18 +4,21 @@ package has2be
 
 import (
 	"context"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 
 	"github.com/thoughtworks/maeve-csms/manager/ocpp"
 	"github.com/thoughtworks/maeve-csms/manager/ocpp/has2be"
-	"golang.org/x/exp/slog"
 )
 
 type CertificateSignedResultHandler struct{}
 
 func (c CertificateSignedResultHandler) HandleCallResult(ctx context.Context, chargeStationId string, request ocpp.Request, response ocpp.Response, state any) error {
+	span := trace.SpanFromContext(ctx)
+
 	resp := response.(*has2be.CertificateSignedResponseJson)
 
-	slog.Info("certificate signed response", slog.Any("status", resp.Status))
+	span.SetAttributes(attribute.String("response.status", string(resp.Status)))
 
 	return nil
 }
