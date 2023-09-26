d89bcb69ba3ca908a27b78f5a17c3bea7bf5e9ec
usuletw022 <usman.sule@thoughtworks.com>
2023-09-26 16:41:12 +0100

    [US] Added base64 encoding and iteration per vu load simulation

--- loadtests/ws_load_test.js
+++ loadtests/ws_load_test.js
@@ -1,18 +1,28 @@
 import ws from 'k6/ws';
 import { check } from 'k6';
+import encoding from 'k6/encoding';
+
 export const options = {
-    vus: 1,
-    duration: '10s',
+    discardResponseBodies: true,
+    scenarios: {
+        contacts: {
+            executor: 'per-vu-iterations',
+            vus: 5,
+            iterations: 5,
+            maxDuration: '20s',
+        },
+    },
 };
 
-const csIds = [ 1, 2, 3, 4, 5];
+const csIds = [1, 2, 3, 4, 5];
+let csId = csIds[Math.floor(Math.random()*csIds.length)];
+let data = `cs00${csId}:fiddlesticks_fishsticks`;
+let base64data = encoding.b64encode(data)
 
 export default function () {
-    let csId = csIds[Math.floor(Math.random()*csIds.length)];
     const params = {
-        headers: {'Sec-WebSocket-Protocol': 'ocpp1.6', 'Authorization': 'Basic Y3MwMDE6ZmlkZGxlc3RpY2tzX2Zpc2hzdGlja3M='}
+        headers: {'Sec-WebSocket-Protocol': 'ocpp1.6', 'Authorization': `Basic ${base64data}`}
     };
-
         const url = `ws://localhost/ws/cs00${csId}`;
         const res = ws.connect(url, params, function (socket) {
         socket.send('[2,"1","BootNotification",{"chargePointModel":"me100","chargePointVendor":"me"}]');
