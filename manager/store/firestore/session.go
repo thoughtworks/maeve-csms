6a82ee31d88f835fe4f7a883027b2a8370cdfef6
laurabassani <laura.bassani@thoughtworks.com>
2023-10-12 14:52:15 +0100

    feat: implement SetSession in store

--- manager/store/firestore/session.go
+++ manager/store/firestore/session.go
@@ -2,11 +2,16 @@ package firestore
 
 import (
 	"context"
+	"fmt"
 	"github.com/thoughtworks/maeve-csms/manager/store"
 )
 
 func (s *Store) SetSession(ctx context.Context, session *store.Session) error {
-
+	sessionRef := s.client.Doc(fmt.Sprintf("Session/%s", session.Id))
+	_, err := sessionRef.Set(ctx, session)
+	if err != nil {
+		return fmt.Errorf("setting session %s: %w", session.Id, err)
+	}
 	return nil
 }
 
