ed4d2fbe6fbde4c12da8e0926226509fbca3f392
usuletw022 <usman.sule@thoughtworks.com>
2024-03-08 17:00:53 +0000

    [US] Change certificate information on the script

--- scripts/get-ca-cert.sh
+++ scripts/get-ca-cert.sh
@@ -10,6 +10,6 @@ certs=$(curl -s https://open.plugncharge-test.hubject.com/cpo/cacerts/ISO15118-2
   -H 'Content-Transfer-Encoding: application/pkcs10' | openssl enc -base64 -d | openssl pkcs7 -inform DER -print_certs)
 
 echo "${certs}" | awk '/subject.*CN.*=.*CPO Sub1 CA QA G1.2/,/END CERTIFICATE/' > "${script_dir}"/../config/certificates/cpo_sub_ca1.pem
-echo "${certs}" | awk '/subject.*CN.*=.*CPO Sub2 CA QA G1.2.1/,/END CERTIFICATE/' > "${script_dir}"/../config/certificates/cpo_sub_ca2.pem
+echo "${certs}" | awk '/subject.*CN.*=.*CPO Sub2 CA QA G1.2.2/,/END CERTIFICATE/' > "${script_dir}"/../config/certificates/cpo_sub_ca2.pem
 echo "${certs}" | awk '/subject.*CN.*=.*V2G Root CA QA G1/,/END CERTIFICATE/' > "${script_dir}"/../config/certificates/root-V2G-cert.pem
 cat "${script_dir}"/../config/certificates/cpo_sub_ca1.pem "${script_dir}"/../config/certificates/cpo_sub_ca2.pem > "${script_dir}"/../config/certificates/trust.pem
