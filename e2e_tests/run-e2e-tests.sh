5eaa933cfb07ffe6b60623fc1acc53c419dbbf8b
usuletw022 <usman.sule@thoughtworks.com>
2024-03-21 19:10:34 +0000

    e2e tests: renamed e2e test folder, updated e2e test script variables and commands

--- e2e_tests/run-e2e-tests.sh
+++ e2e_tests/run-e2e-tests.sh
@@ -1,23 +1,49 @@
 #!/bin/bash
 
+# Get the directory where the script is located
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+# Get the directory where the CSMS is located
+DEFAULT_CSMS_DIR="${SCRIPT_DIR}"/../../maeve-csms
+CSMS_DIR="${1:-$DEFAULT_CSMS_DIR}"
+
+# Define paths relative to the script's location
+EVEREST_DIR="$CSMS_DIR/e2e_tests"
+TEST_DIR="$CSMS_DIR/e2e_tests/test_driver"
+
+
 # Function to start Docker Compose
-start_docker_compose() {
-    cd . && docker-compose up -d
+start_docker_compose_for_maeve_csms() {
+    cd "$CSMS_DIR" && docker-compose up -d
     if [ $? -eq 0 ]; then
         echo "Docker Compose started successfully"
     else
         echo "Failed to start Docker Compose"
+        stop_docker_compose_for_maeve_csms
         exit 1
     fi
 }
 
+# Function to start Docker Compose
+start_docker_compose_for_everest() {
+        cd "$EVEREST_DIR" && make up
+        if [ $? -ne 0 ]; then
+            echo "Failed to start Docker Compose for tests"
+            stop_docker_compose_for_everest
+            exit 1
+        fi
+
+        echo "Waiting for services to initialize..."
+        sleep 20
+}
+
 # Function to stop Docker Compose
 stop_docker_compose_for_everest() {
-    cd e2e-tests && docker-compose down
+    cd "$EVEREST_DIR" && docker-compose down
 }
 
 stop_docker_compose_for_maeve_csms() {
-    cd .. && docker-compose down
+    cd "$CSMS_DIR" && docker-compose down
 }
 
 # Function to check health endpoint
@@ -39,20 +65,9 @@ check_health_endpoint() {
 
 # Function to run tests
 run_tests() {
-    cd e2e-tests
-    make up
-    if [ $? -ne 0 ]; then
-        echo "Failed to start Docker Compose for tests"
-        stop_docker_compose_for_everest
-        exit 1
-    fi
-
-    echo "Waiting for services to initialize..."
-    sleep 20
-
     echo "Running test command..."
-    cd test-driver
-    go test -v ./... -count=1
+    cd "$TEST_DIR"
+    go test --tags=e2e -v ./... -count=1
     TEST_RESULT=$?
     cd ../..
 
@@ -67,6 +82,7 @@ run_tests() {
 }
 
 # Main script execution
-start_docker_compose
+start_docker_compose_for_maeve_csms
 check_health_endpoint
+start_docker_compose_for_everest
 run_tests
