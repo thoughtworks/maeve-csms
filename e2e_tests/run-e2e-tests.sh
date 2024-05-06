55c65bba34fe89acbbd4830d6f65be914dc6d053
Dale Peakall <dpeakall@thoughtworks.com>
2024-05-06 14:58:55 +0100

    build: set uid and gid
    So it looks UID/GID environment variables aren't set in the GitHub action environment.

--- e2e_tests/run-e2e-tests.sh
+++ e2e_tests/run-e2e-tests.sh
@@ -17,6 +17,8 @@ start_docker_compose_for_maeve_csms() {
     cd "$CSMS_DIR"
     (cd config/certificates && make)
     chmod 755 $CSMS_DIR/config/certificates/csms.key
+    export UID=$(id -u)
+    export GID=$(id -g)
     docker-compose up -d
     if [ $? -eq 0 ]; then
         echo "Docker Compose started successfully"
